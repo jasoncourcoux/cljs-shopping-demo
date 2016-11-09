621b40389df39a4529ffc364b23c7658c93de61d
Jason Courcoux <jxc1206@googlemail.com>
2016-11-09 19:01:16 +0000

    Removing out files

--- resources/public/cljs/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [shopping-demo.app] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/shopping-demo.app.load (apply js/shopping-demo.app.load x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'shopping-demo.app/load' is missing"))), :build-id "dev", :websocket-url "ws://localhost:3448/figwheel-ws"})
-
--- resources/public/cljs/out/shopping_demo/component/header.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.9.293 {}
-goog.provide('shopping_demo.component.header');
-goog.require('cljs.core');
-
-//# sourceMappingURL=header.js.map
